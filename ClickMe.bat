93615d9d120da61b8b70bd0c00640b8e1a301c77
flaykDev <138144836+Flaykky@users.noreply.github.com>
2025-09-12 21:23:38 +0300

    Update ClickMe.bat

--- ClickMe.bat
+++ ClickMe.bat
@@ -1,3 +1,34 @@
-@echo off 
+@echo off
 echo loading...
-python3 ui/python/main.py
+
+:: моя проверка про которую я говорил
+where python >nul 2>nul
+if errorlevel 1 (
+    echo Python is not installed in PATH, please install it.
+    pause
+    exit /b
+)
+
+for /f "tokens=2 delims= " %%a in ('python --version 2^>^&1') do set PYVER=%%a
+
+echo Found Python version %PYVER%
+
+for /f "tokens=1,2 delims=." %%i in ("%PYVER%") do (
+    set MAJOR=%%i
+    set MINOR=%%j
+)
+
+if %MAJOR% LSS 3 (
+    echo Python version is not compatible. Please install Python 3.7+.
+    pause
+    exit /b
+)
+if %MAJOR%==3 if %MINOR% LSS 7 (
+    echo Python version is not compatible. Please install Python 3.7+.
+    pause
+    exit /b
+)
+
+echo Starting ui/python/main.py...
+python ui/python/main.py
+pause
