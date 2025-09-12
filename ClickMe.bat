8ed9478196d709b4a45312f73a6eca20162c0e6a
stdio.h <sanacitata532@gmail.com>
2025-09-12 17:50:09 +0000

    Update ClickMe.bat

--- ClickMe.bat
+++ ClickMe.bat
@@ -1,15 +1,3 @@
-@echo off
-cd /d "%~dp0"
-
-set "FILE=main.py"
-
-if exist "%PYFILE%" (
-    echo START %fILE%
-    python "%FILE%"
-) else (
-    echo ERROR 404!
-    echo :
-    dir /b *.py
-)
-
-pause
+@echo off 
+echo loading...
+python3 ui/python/main.py
