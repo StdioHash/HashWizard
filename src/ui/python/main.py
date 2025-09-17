e6aa17b2a52165d40e9c751418d31c7aec884c24
stdio.h <sanacitata532@gmail.com>
2025-09-17 07:58:20 +0000

    main.py

--- src/ui/python/main.py
+++ src/ui/python/main.py
@@ -1,2 +1,28 @@
-print("hello")
+import importlib.util
+import subprocess
+import sys
 
+packages = ["termcolor", "colorama"]
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+for package in packages:
+    if importlib.util.find_spec(package) is None:
+        print(f"{package} not installed")
+        install(package)
+
+#libs
+import colorama
+import os
+import termcolor 
+from termcolor import colored
+
+
+#colorama
+from colorama import init
+init(autoreset=True)
+from colorama import Fore, Back
+
+
+print(colored(">HashWizard. LOADING...", "black", "on_yellow"))
